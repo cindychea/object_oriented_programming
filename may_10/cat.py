ae055155ef5a0c58762ef9e23e615f7930b090ca
cindychea <cindy_chea@hotmail.com>
2019-05-10 14:21:51 -0400

    better completed version of cat exercise, minor changes made to add method inside method

--- may_10/cat.py
+++ may_10/cat.py
@@ -9,29 +9,29 @@ class Cat:
         return "{} has his meal at {}:00 h and likes to eat {}.".format(self.name, self.meal_time, self.preferred_food)
     
     def eats_at(self):
-        if self.meal_time <= 11:
-            return "{} eats at {} AM.".format(self.name, self.meal_time)
-        elif self.meal_time <= 23:
-            self.meal_time = self.meal_time - 12
-            return "{} eats at {} PM.".format(self.name, self.meal_time)
+        if self.meal_time >= 24:
+            return None
+        elif 23 <= self.meal_time >= 12:
+            self.meal_time = abs(self.meal_time - 12)
+            return "{} PM".format(self.meal_time)
         else:
-            return "{} does not seem to have a valid meal time.".format(self.name)
+            return "{} AM".format(self.meal_time)
 
     def meow(self):
-        return "Meow! My name is {} and I eat {} at {}:00 h! PLEASE FEED ME OR ELSE.".format(self.name, self.preferred_food, self.meal_time)
+        return "Meow! My name is {} and I eat {} at {}! PLEASE FEED ME OR ELSE.".format(self.name, self.preferred_food, self.eats_at())
 
 cat1 = Cat('Nemo', 'tuna', 7)
 cat2 = Cat('Lenny', 'salmon', 23)
 cat3 = Cat('Roger', 'kale', 25)
 
-print(cat1)
-print(cat1.eats_at())
+# print(cat1)
+# print(cat1.eats_at())
 print(cat1.meow())
-print(cat2)
-print(cat2.eats_at())
+# print(cat2)
+# print(cat2.eats_at())
 print(cat2.meow())
-print(cat3)
-print(cat3.eats_at())
+# print(cat3)
+# print(cat3.eats_at())
 print(cat3.meow())
 
-# Is there a way to get the result from eats_at function and put it in meow function?
+# MEOW seems to be calling the function again and the time is not right????
